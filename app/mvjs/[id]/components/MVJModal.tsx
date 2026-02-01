26533e5a0cb68463a96585937e04496501d25205
chihatw <chiha.tw@gmail.com>
2026-02-01 10:21:07 +0900

    MVJ メッセージ修正

--- app/mvjs/[id]/components/MVJModal.tsx
+++ app/mvjs/[id]/components/MVJModal.tsx
@@ -21,15 +21,17 @@ export const MVJModal = ({
         </h2>
         <section className='text-sm leading-4 p-5 space-y-4 font-light tracking-wide'>
           <p>
-            我們要閱讀「過去留給未來的自己、可以用在對話上的日語」，也就是學習日誌。
+            回頭看看自己寫過的學習日誌吧！
+            <br />
+            不回頭整理一下，很快就會忘記，寫了就等於白寫。
           </p>
 
           <ol className='list-decimal pl-6 space-y-2'>
             <li>重新閱讀自己的學習日誌。</li>
             <li>
-              選出你認為「最能用在真實對話中使用的內容」為 「
-              <span className='marker-underline-yellow'>最佳作品</span>」。
-              若還有覺得能派上用場的內容，可標為 「
+              選出你認為「最能讓對方感受到你的心意、能縮短彼此距離的內容」為「
+              <span className='marker-underline-yellow'>最佳作品</span>
+              」。若還有覺得在真實對話中也能自然說出口的內容，可標為「
               <span className='marker-underline-yellow'>佳作</span>」。
             </li>
             <li>
@@ -54,23 +56,12 @@ export const MVJModal = ({
             <div className='mb-1 flex'>評選基準</div>
             <div className='flex'>
               <div className='font-extrabold tracking-tighter text-base'>
-                能清楚想像「自己在什麼情境、對誰說出這句日語」。
+                能清楚想像「自己想把這句話對誰說、說出口時能讓彼此距離變近的情境」。
               </div>
             </div>
           </div>
 
-          <p>希望你學到的日語，不只是知識，而是能在生活中運用的能力。</p>
-
-          <div className='pt-2 border-t'>
-            <div className='mb-2'>例）</div>
-            <ul className='list-disc pl-6 space-y-1.5'>
-              <li>在被朋友介紹時，能向第一次見面的人打招呼。</li>
-              <li>在餐廳用餐時，能對店員追加點餐。</li>
-              <li>在和朋友聊天時，能表達自己的心情。</li>
-              <li>在看到需要幫助的人時，能主動上前說話。</li>
-              <li>在旅行結束後，能向日本人分享旅行中看到的事物。</li>
-            </ul>
-          </div>
+          <p>希望你學到的日語，是能真正用來與人交流心意的話語。</p>
         </section>
         <div className='px-4 flex'>
           <label className='flex items-center gap-2 text-xs px-4 bg-slate-50 border py-2 rounded-full'>
