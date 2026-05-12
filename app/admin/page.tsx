79d3e42fa932ef86f13ed0e4d8ee85e919b36b95
chihatw <chiha.tw@gmail.com>
2026-05-12 11:49:54 +0900

    PR Test

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -22,6 +22,9 @@ export default function AdminHomePage() {
           </div>
         </Link>
       ))}
+      <div className='text-center font-bold text-2xl text-gray-300'>
+        PR TEST
+      </div>
     </div>
   );
 }
