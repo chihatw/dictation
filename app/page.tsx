395ce7220d86c47b974b619252190102a406e855
chihatw <chiha.tw@gmail.com>
2026-05-13 13:17:33 +0900

    ルートページで未ログインユーザーにランディングページを表示

--- app/page.tsx
+++ app/page.tsx
@@ -2,30 +2,18 @@
 export const dynamic = 'force-dynamic';
 import { createClient } from '@/lib/supabase/server';
 
-import { HomeClozeContainer } from '@/components/home/cloze/HomeClozeContainer';
-import { HomeJournalsContainer } from '@/components/home/journals/HomeJournalsContainer';
-import { HomeMVJContainer } from '@/components/home/mvj/HomeMVJContainer';
-import { NextClassContainer } from '@/components/home/nextClass/NextClassContainer';
-import { HomePowerIndexContainer } from '@/components/home/powerIndex/HomePowerIndexContainer';
-import TodayPanelContainer from '@/components/home/todayPanel/TodayPanelContainer';
+import { UserHome } from '@/components/home/UserHome';
+import { LandingPage } from '@/components/landing/LandingPage';
 
 export default async function Home() {
   const supabase = await createClient();
   const {
     data: { user },
   } = await supabase.auth.getUser();
-  if (!user) throw new Error('unauthorized');
 
-  return (
-    <div className='min-h-screen p-6'>
-      <main className='mx-auto max-w-2xl space-y-6'>
-        <TodayPanelContainer />
-        <NextClassContainer userId={user.id} />
-        <HomeMVJContainer userId={user.id} />
-        <HomePowerIndexContainer userId={user.id} />
-        <HomeClozeContainer userId={user.id} />
-        <HomeJournalsContainer userId={user.id} />
-      </main>
-    </div>
-  );
+  if (!user) {
+    return <LandingPage />;
+  }
+
+  return <UserHome userId={user.id} />;
 }
